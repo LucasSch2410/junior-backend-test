c752a286f90e09b727f78d8b225bcf7360353c24
Lucas Schroeder <lucasschroeder2410@gmail.com>
2025-07-03 14:07:05 -0300

    chore: initial setup with Vue and Inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/js/app.js'])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased bg-gray-50">
+    @inertia
+</body>
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use App\Http\Controllers\CreateContactController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Contacts');
 });
 
 Route::post('/contacts', CreateContactController::class);
